8db116a8fa922d61674a623ecdd55eb366e2709d
Phongle1311 <hoaiphong13.11.2002@gmail.com>
2022-11-16 12:46:28 +0700

    https://lhphong.atlassian.net/browse/ANDROID05-109 add activity load and display image fullscreen, can scale image

---
+++ app/src/main/java/com/hcmus/picbox/DisplayMediaActivity.java
@@ -0,0 +1,63 @@
+package com.hcmus.picbox;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.hcmus.picbox.models.PhotoModel;
+
+/**
+ * Created on 16/11/2022 by Phong Le
+ */
+public class DisplayMediaActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+    private ScaleGestureDetector scaleGestureDetector;
+    private float mScaleFactor = 1.0f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_media);
+
+        mImageView = findViewById(R.id.image_view);
+
+        PhotoModel model = (PhotoModel) getIntent().getSerializableExtra("model");
+        if (model.checkExists()) {
+            Glide
+                    .with(DisplayMediaActivity.this)
+                    .load(model.getFile())
+                    .placeholder(R.drawable.placeholder_color)
+                    .error(R.drawable.placeholder_color) // TODO: replace by other drawable
+                    .into(mImageView);
+        }
+
+        scaleGestureDetector = new ScaleGestureDetector(this, new CustomizeScaleListener());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        scaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
+    }
+
+    /**
+     * Listen and handle scale event
+     */
+    private class CustomizeScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 10.0f));
+
+            mImageView.setScaleX(mScaleFactor);
+            mImageView.setScaleY(mScaleFactor);
+            return true;
+        }
+    }
+}

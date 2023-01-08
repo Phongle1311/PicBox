19df2e519f306a39ad83796c0b1c591a2d409d92
QuangHuy54 <73436308+QuangHuy54@users.noreply.github.com>
2023-01-09 02:49:42 +0700

    ANDROID05-229 Add adjust speed and play/pause.

--- app/src/main/java/com/hcmus/picbox/activities/SlideShowActivity.java
+++ app/src/main/java/com/hcmus/picbox/activities/SlideShowActivity.java
@@ -1,11 +1,16 @@
 package com.hcmus.picbox.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.tabs.TabLayout;
 import com.hcmus.picbox.R;
 import com.hcmus.picbox.models.AlbumModel;
 import com.hcmus.picbox.models.MediaModel;
@@ -16,15 +21,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 import kotlin.jvm.functions.Function2;
 import technolifestyle.com.imageslider.FlipperLayout;
 import technolifestyle.com.imageslider.FlipperView;
+import technolifestyle.com.imageslider.pagetransformers.DepthPageTransformer;
+import technolifestyle.com.imageslider.pagetransformers.ZoomOutPageTransformer;
 
 public class SlideShowActivity extends AppCompatActivity {
-
+    private final String speeds[] = {"0.25x", "0.5x", "1x", "2x"};
     private ImageView btnBack;
     private ImageView btnReplay;
     private FlipperLayout flipperLayout;
+    private ImageView btnPlayPause;
+    private boolean isPlaying = true;
+    private int scrollTime = 2;
+    private Spinner spinnerChooseSpeed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +55,6 @@ public class SlideShowActivity extends AppCompatActivity {
         for (Integer id : selected_media_id)
             medias.add(album.findMediaById(id));
 
-
         for (MediaModel media : medias) {
             FlipperView view = new FlipperView(SlideShowActivity.this);
             try {
@@ -61,13 +72,51 @@ public class SlideShowActivity extends AppCompatActivity {
             view.setDescriptionBackgroundAlpha(0f);
             flipperLayout.addFlipperView(view);
         }
-        flipperLayout.setScrollTimeInSec(3);
-        btnReplay.setOnClickListener(v -> flipperLayout.onCurrentPageChanged(0));
+
+        flipperLayout.setScrollTimeInSec(scrollTime);
+        setListener();
     }
 
     private void bindUI() {
         btnBack = findViewById(R.id.img_slider_back);
         btnReplay = findViewById(R.id.img_slider_replay);
         flipperLayout = (FlipperLayout) findViewById(R.id.flipper_layout);
+        btnPlayPause = findViewById(R.id.img_slider_play_pause);
+        spinnerChooseSpeed = (Spinner) findViewById(R.id.spinner_choose_speed);
+    }
+
+    private void setListener() {
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, speeds);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerChooseSpeed.setAdapter(spinnerArrayAdapter);
+        spinnerChooseSpeed.setSelection(2);
+        btnReplay.setOnClickListener(v -> flipperLayout.onCurrentPageChanged(0));
+        btnPlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPlaying) {
+                    flipperLayout.removeAutoCycle();
+                    isPlaying = false;
+                    btnPlayPause.setImageResource(R.drawable.ic_baseline_play_24);
+                } else {
+                    flipperLayout.startAutoCycle(scrollTime);
+                    isPlaying = true;
+                    btnPlayPause.setImageResource(R.drawable.ic_baseline_pause_24);
+                }
+
+            }
+        });
+        spinnerChooseSpeed.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                scrollTime = (1 << (3 - position));
+                flipperLayout.setScrollTimeInSec(scrollTime);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }

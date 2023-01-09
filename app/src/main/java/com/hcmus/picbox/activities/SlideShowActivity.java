16c1404b49a122ba2e2173a0eab25c764e56a1db
QuangHuy54 <73436308+QuangHuy54@users.noreply.github.com>
2023-01-09 13:12:00 +0700

    ANDROID05-229 Add choose audio file to play music.

--- app/src/main/java/com/hcmus/picbox/activities/SlideShowActivity.java
+++ app/src/main/java/com/hcmus/picbox/activities/SlideShowActivity.java
@@ -1,12 +1,21 @@
 package com.hcmus.picbox.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
@@ -29,7 +38,7 @@ import technolifestyle.com.imageslider.pagetransformers.DepthPageTransformer;
 import technolifestyle.com.imageslider.pagetransformers.ZoomOutPageTransformer;
 
 public class SlideShowActivity extends AppCompatActivity {
-    private final String speeds[] = {"0.25x", "0.5x", "1x", "2x"};
+    private final String[] speeds = {"0.25x", "0.5x", "1x", "2x"};
     private ImageView btnBack;
     private ImageView btnReplay;
     private FlipperLayout flipperLayout;
@@ -37,6 +46,8 @@ public class SlideShowActivity extends AppCompatActivity {
     private boolean isPlaying = true;
     private int scrollTime = 2;
     private Spinner spinnerChooseSpeed;
+    private MediaPlayer player;
+    private ImageView btnChooseMusic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,11 +70,7 @@ public class SlideShowActivity extends AppCompatActivity {
             FlipperView view = new FlipperView(SlideShowActivity.this);
             try {
                 view.setImage(R.drawable.placeholder_color, (imageView, o) -> {
-                    Glide.with(SlideShowActivity.this)
-                            .load(media.getFile())
-                            .placeholder(R.drawable.placeholder_color)
-                            .error(R.drawable.placeholder_color)
-                            .into(imageView);
+                    Glide.with(SlideShowActivity.this).load(media.getFile()).placeholder(R.drawable.placeholder_color).error(R.drawable.placeholder_color).into(imageView);
                     return Unit.INSTANCE;
                 });
             } catch (MalformedURLException e) {
@@ -83,6 +90,7 @@ public class SlideShowActivity extends AppCompatActivity {
         flipperLayout = (FlipperLayout) findViewById(R.id.flipper_layout);
         btnPlayPause = findViewById(R.id.img_slider_play_pause);
         spinnerChooseSpeed = (Spinner) findViewById(R.id.spinner_choose_speed);
+        btnChooseMusic = findViewById(R.id.img_choose_music);
     }
 
     private void setListener() {
@@ -90,16 +98,22 @@ public class SlideShowActivity extends AppCompatActivity {
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerChooseSpeed.setAdapter(spinnerArrayAdapter);
         spinnerChooseSpeed.setSelection(2);
-        btnReplay.setOnClickListener(v -> flipperLayout.onCurrentPageChanged(0));
+        btnReplay.setOnClickListener(v -> {
+            flipperLayout.onCurrentPageChanged(0);
+            player.seekTo(0);
+            player.start();
+        });
         btnPlayPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (isPlaying) {
                     flipperLayout.removeAutoCycle();
                     isPlaying = false;
+                    player.pause();
                     btnPlayPause.setImageResource(R.drawable.ic_baseline_play_24);
                 } else {
                     flipperLayout.startAutoCycle(scrollTime);
+                    player.start();
                     isPlaying = true;
                     btnPlayPause.setImageResource(R.drawable.ic_baseline_pause_24);
                 }
@@ -115,8 +129,27 @@ public class SlideShowActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
+        btnChooseMusic.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+            fileChooseActivity.launch(intent);
+        });
     }
+
+    ActivityResultLauncher<Intent> fileChooseActivity = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Intent resultData = result.getData();
+                Uri uri = null;
+                if (resultData != null) {
+                    uri = resultData.getData();
+                    player = MediaPlayer.create(getBaseContext(), uri);
+                    player.setLooping(true);
+                    player.start();
+                }
+            }
+        }
+    });
 }
